7805f1741dd625e1e7619e6c41cac914148fd917
Daniel Friesel <derf@finalrewind.org>
2013-07-27 18:13:17 +0200

    initial

---
+++ index.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use 5.014;
+use utf8;
+
+use List::MoreUtils qw(firstidx);
+use Mojolicious::Lite;
+use Mojolicious::Static;
+use File::Slurp qw(read_dir slurp);
+
+our $VERSION = '0.00';
+my $prefix = '/home/derf/lib';
+
+get '/*path' => sub {
+	my $self = shift;
+	my $path = $self->stash('path');
+
+	my ($dir, $file) = ( $path =~ m{ ^ (.+) / ([^/]+) \. html $ }ox );
+
+	if ($path =~ m{ \. html $ }ox) {
+
+		my @all_files = read_dir("${prefix}/${dir}");
+		@all_files = grep { -f "${prefix}/${dir}/$_" } sort @all_files;
+		my $idx = firstidx { $_ eq $file } @all_files;
+
+		my $prev_idx = ($idx == 0 ? 0 : $idx - 1);
+		my $next_idx = ($idx == $#all_files ? $idx : $idx + 1);
+
+		$self->render('main',
+			prev => $all_files[$prev_idx] . '.html',
+			next => $all_files[$next_idx] . '.html',
+			random => $all_files[int(rand($#all_files))] . '.html',
+			parent => $dir,
+			file => $file,
+		);
+	}
+	elsif (-d "${prefix}/${path}") {
+		$path =~ s{ / $ }{}ox;
+		my @all_files = read_dir("${prefix}/${path}", keep_dot_dot => 1);
+		@all_files = map { [$_, -d "${prefix}/${path}/$_" ? "/${path}/$_" : "/${path}/$_.html"] } sort @all_files;
+		$self->render('list',
+			files => \@all_files,
+		);
+	}
+	else {
+		$self->render_static($path);
+	}
+
+};
+
+app->config(
+	hypnotoad => {
+		listen          => ['http://*:8094'],
+		pid_file        => '/tmp/ithumb.pid',
+		workers         => 1,
+	},
+);
+
+app->defaults( layout => 'default' );
+push(@{app->static->paths}, $prefix);
+
+app->start();
